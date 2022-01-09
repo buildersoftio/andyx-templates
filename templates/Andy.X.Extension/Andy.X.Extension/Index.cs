07c7f9aaa471796e059693303cd8a355341a4165
Enes Hoxha <eneshoxha@outlook.com>
2022-01-09 20:18:33 +0100

    Add project files.

---
+++ templates/Andy.X.Extension/Andy.X.Extension/Index.cs
@@ -0,0 +1,13 @@
+﻿using Andy.X.Client;
+using System;
+
+namespace Andy.X.Extension
+{
+    public static class Index
+    {
+        public static void Main(XClient xClient, object data)
+        {
+            Console.WriteLine("Hello World from Andy X Extension");
+        }
+    }
+}
